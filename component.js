0fc9d0fa4063126af0f91fca88ad2ff52c9750af
aalixue <2319684501@qq.com>
2020-03-24 14:33:54 +0800

    .

---
+++ component.js
@@ -0,0 +1,133 @@
+// handsontable
+var dataObject = [
+    {
+        name: 'Java',
+        rank: 1,
+        aod: '降',
+        change: -0.0001
+    },
+    {
+        name: 'C',
+        rank: 2,
+        aod: '升',
+        change: 0.0244
+    },
+    {
+        name: 'Python',
+        rank: 3,
+        aod: '升',
+        change: 0.0141
+    },
+    {
+        name: 'C++',
+        rank: 4,
+        aod: '降',
+        change: -0.0285
+    },
+    {
+        name: 'C#',
+        rank: 5,
+        aod: '升',
+        change: 0.0207
+    },
+    {
+        name: 'Visual Basic .NET',
+        rank: 6,
+        aod: '降',
+        change: -0.0117
+    },
+    {
+        name: 'JavaScript',
+        rank: 7,
+        aod: '降',
+        change: -0.0085
+    }
+];
+
+var hotElement = document.getElementById('hot');
+var hotSettings = {
+    data: dataObject,
+    columns: [
+        {
+            data: 'name',
+            type: 'text'
+        },
+        {
+            data: 'rank',
+            type: 'numeric'
+        },
+        {
+            data: 'aod',
+            type: 'text'
+        },
+        {
+            data: 'change',
+            type: 'numeric',
+            numericFormat: {
+                pattern: '+0.00%'
+            }
+        }
+    ],
+    stretchH: 'all',
+    width: 700,
+    autoWrapRow: true,
+    height: 400,
+    maxRows: 22,
+    colHeaders: [
+        '语言名称',
+        '排名',
+        '升或降',
+        '变化幅度'
+    ],
+    rowHeights: 40,
+    colWidths: 50,
+    className: "htCenter htMiddle",
+    exportFile: true,
+    mergeCells: true,
+    contextMenu: true,
+    manualRowResize: true,
+    manualColumnMove: true,
+    manualRowMove: true,
+    manualColumnResize: true,
+    filters: true,
+    contextMenu: true,
+    hiddenColumns: true,
+    hiddenColumns: true,
+};
+var hot = new Handsontable(hotElement, hotSettings);
+
+//echarts
+var myChart = echarts.init(document.getElementById('main'));
+
+var option = {
+    title: {
+        text: 'JavaScript语言排名变化'
+    },
+    tooltip: {
+        show: true,
+        axisPointer: {
+            type: 'line',
+            lineStyle: {
+                color: '#eeeeee',
+                width: 1
+            }
+        }
+    },
+    legend: {
+        data: ['排名']
+    },
+    xAxis: {
+        data: ["2000", "2005", "2010", "2015", "2020"],
+        axisPointer: {
+            show: true
+        }
+    },
+    yAxis: {},
+    series: [{
+        name: '排名',
+        type: 'line',
+        data: [6, 9, 8, 8, 7]
+    }]
+};
+
+myChart.setOption(option);
